2d0716b6152b49c64a5d6983ea4c3d273e5ef9e3
Ivan <ivanteozhiming@gmail.com>
2023-08-27 13:19:14 +0800

    landing

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 
-export default function Button({title, onClick}: {title: string, onClick?: ()=>void}) {
+export default function Button({
+  title,
+  isSecondary,
+  className,
+  onClick,
+}: {
+  title: string;
+  isSecondary?: boolean;
+  className?: string;
+  onClick?: () => void;
+}) {
+  const primaryButtonStyle =
+    "bg-red hover:bg-red-hover active:bg-red-active text-white";
+  const secondaryButtonStyle =
+    "bg-white hover:bg-red-hover active:bg-red-active";
+
   return (
-    <button onClick={onClick} className="text-white rounded-md bg-red py-2 px-8 hover:bg-red-hover transition active:bg-red-active active:transition-none ">
+    <button
+      onClick={onClick}
+      className={`rounded-md border-2 border-red py-2 px-8 transition active:transition-none ${className} ${
+        isSecondary ? secondaryButtonStyle : primaryButtonStyle
+      }`}
+    >
       {title}
     </button>
   );
